9dd5e69a8e2c15624b27753ff0234eb49e2e9759
Antonio Tedeschi <antedesk@gmail.com>
2017-12-22 11:33:44 +0100

    Re-inizializzazione repo per iOS su Objective-C.

---
+++ Objective-C/SocialLoginHTMLit/SocialLoginHTMLit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SocialLoginHTMLit
+//
+//  Created by antonio.tedeschi on 22/12/17.
+//  Copyright © 2017 antedesk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Objective-C/SocialLoginHTMLit/SocialLoginHTMLit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SocialLoginHTMLit
+//
+//  Created by antonio.tedeschi on 22/12/17.
+//  Copyright © 2017 antedesk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
