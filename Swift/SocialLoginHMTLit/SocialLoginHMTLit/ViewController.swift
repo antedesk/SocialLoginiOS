b1a6a49ba6eba7a2801e92175cdfdf94a53a388b
Antonio Tedeschi <antedesk@gmail.com>
2018-02-02 19:17:40 +0100

    Aggiunto bottone di login e logout.

--- Swift/SocialLoginHMTLit/SocialLoginHMTLit/ViewController.swift
+++ Swift/SocialLoginHMTLit/SocialLoginHMTLit/ViewController.swift
@@ -7,13 +7,30 @@
 //
 
 import UIKit
+import FBSDKLoginKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController , FBSDKLoginButtonDelegate {
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        print("Did log out of facebook")
+    }
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        if error != nil {
+            print(error)
+            return
+        }
+        
+        print("Successfully logged in with facebook...")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let loginButton = FBSDKLoginButton()
+        view.addSubview(loginButton)
+        //frame's are obselete, please use constraints instead because its 2016 after all
+        loginButton.frame = CGRect(x: 16, y: 50, width: view.frame.width - 32, height: 50)
+        
+        loginButton.delegate = self
     }
-
 }
-
