4460a04f3b29355987b6d797f44098742952f67e
Antonio Tedeschi <antedesk@gmail.com>
2018-02-09 19:15:03 +0100

    Aggiunto bottone di logout e  gestione della label per la stampa del nome utente.

--- Swift/SocialLoginHMTLit/SocialLoginHMTLit/ViewController.swift
+++ Swift/SocialLoginHMTLit/SocialLoginHMTLit/ViewController.swift
@@ -10,24 +10,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var logOutButton: UIButton!
+    @IBOutlet weak var userIdLabel: UILabel!
+    var logInButton: TWTRLogInButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let logInButton = TWTRLogInButton(logInCompletion: { session, error in
+        logInButton = TWTRLogInButton(logInCompletion: { session, error in
             if (session != nil) {
-                print("\nsigned in as \(session?.userName)");
+                if let userName = session?.userName{
+                    print("hi \(userName)");
+                    self.userIdLabel.text = "Hi \(userName)!"
+                }
+                self.toggleAuthUI()
             } else {
-                print("\nerror: \(error?.localizedDescription)");
+                print("error: \(error?.localizedDescription)");
             }
         })
         logInButton.center = self.view.center
         self.view.addSubview(logInButton)
+        toggleAuthUI()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func didTapLogOut(_ sender: AnyObject) {
+        
+        let store = Twitter.sharedInstance().sessionStore
+        
+        if let userID = store.session()?.userID {
+            store.logOutUserID(userID)
+        }
+        print("[LogOut] - User has session: \(Twitter.sharedInstance().sessionStore.hasLoggedInUsers())")
+        toggleAuthUI()
+    }
 
+    func toggleAuthUI() {
+        print("[Toggle] - User has session: \(Twitter.sharedInstance().sessionStore.hasLoggedInUsers())")
+        if (Twitter.sharedInstance().sessionStore.hasLoggedInUsers()) {
+            // Signed in
+            print("\nUser Session\n")
+            logInButton.isHidden = true
+            logOutButton.isHidden = false
+        } else {
+            print("\nNo User session\n")
+            logInButton.isHidden = false
+            logOutButton.isHidden = true
+            userIdLabel.text = "Twitter Login"
+        }
+    }
 
 }
 
